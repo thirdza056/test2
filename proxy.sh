ed0de2d318b24a98f2ee81127b1d99e1fa18a076
thirdza056 <33078689+thirdza056@users.noreply.github.com>
2018-03-01 14:39:14 +0700

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -1,30 +1,8 @@
 #!/bin/bash
 
-#myip=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
-#myint=`ifconfig | grep -B1 "inet addr:$myip" | head -n1 | awk '{print $1}'`;
 
-#info
-#echo 
-#" INTSALL SQUID3/PROXY ONLY"
-#echo 
-#" AUTOSCRIPT START 1%"
-
-#update
-apt-get install squid3 apache2-utils
-#apt-get -y install wget
-#clear
-
-#echo 
-#" 27% COMPLETE "
-
-#installing squid3
-#apt-get -y install squid3
-#rm -f #/etc#/squid3/squid.#conf
-
-#restoring squid config with open port proxy 8080,7166
-cp /etc/squid3/squid.conf /etc/squid3/squid.conf.orig
-wget -P /etc/squid3/ "https://raw.githubusercontent.com/test2/script/master/script/squid.conf"
-#cp /etc/squid3/squid.conf /etc/squid3/squid.conf.orig
+# install squid3
+apt-get -y install squid3
+wget -O /etc/squid3/squid.conf "https://raw.github.com/test2/script/master/squid3.conf"
+sed -i $MYIP2 /etc/squid3/squid.conf;
 service squid3 restart
-cd
-
