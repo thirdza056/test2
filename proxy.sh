8c71b02c2b63891f986a0a6626797ab4266c68c6
thirdza056 <33078689+thirdza056@users.noreply.github.com>
2018-03-01 14:12:13 +0700

    Add files via upload

---
+++ proxy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#myip=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
+#myint=`ifconfig | grep -B1 "inet addr:$myip" | head -n1 | awk '{print $1}'`;
+
+#info
+#echo 
+#" INTSALL SQUID3/PROXY ONLY"
+#echo 
+#" AUTOSCRIPT START 1%"
+
+#update
+apt-get update
+apt-get -y install wget
+clear
+
+echo 
+" 27% COMPLETE "
+
+#installing squid3
+apt-get -y install squid3
+#rm -f #/etc#/squid3/squid.#conf
+
+#restoring squid config with open port proxy 8080,7166
+wget -P /etc/squid3/ "https://raw.githubusercontent.com/test2/script/master/script/squid.conf"
+cp /etc/squid3/squid.conf /etc/squid3/squid.conf.orig
+service squid3 restart
+cd
+
